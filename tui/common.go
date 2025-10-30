a7c7d2fb4712f1b98c3a89bdeecc761410f0e7ce
Joshua Watkins <126752199+joshuawatkins04@users.noreply.github.com>
2025-10-30 21:00:48 +1100

    new build

---
+++ tui/common.go
@@ -0,0 +1,15 @@
+package tui
+
+import (
+	"io"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+func InitCommonStyles(out io.Writer) {
+	r := lipgloss.NewRenderer(out)
+	helpStyleTUI = r.NewStyle().Foreground(lipgloss.Color("8")).Italic(true)
+	errorStyleTUI = r.NewStyle().Foreground(lipgloss.Color("#FF5555")).Bold(true)
+	warningStyleTUI = r.NewStyle().Foreground(lipgloss.Color("#FFB86C")).Bold(true)
+	successStyle = r.NewStyle().Foreground(lipgloss.Color("#00D787")).Bold(true)
+}
